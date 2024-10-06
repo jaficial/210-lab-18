4338236fe0d7bda322b83e559b4093fb0dcc1b75
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-05 17:10:03 -0700

    10-minute update

--- movie_reviews.cpp
+++ movie_reviews.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 
 using namespace std;
+
+/* Need to incorporate strings into linked lists in the form of movie reviews.
+   Code needs to be able to add nodes to the head and tail of the linked list.
+   At startup, the program asks which mode the user would like to use (adding by head or tail)
+   There should be 2 pieces of data obtained (rating and the comments/review of the movie)*/
+
+// CITED: Will be reusing some of my code from my previous lab submission (Lab 17) for linked list functions
+
+struct Node {
+    float value;
+    Node *next;
+};
+
+void output(Node *);
+void delete_node()
